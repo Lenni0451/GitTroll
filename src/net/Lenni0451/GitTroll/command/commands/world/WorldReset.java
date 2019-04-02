15e56594c07ce6eb8066a0c7dc3e15649c16e5e4
User <lenni0451@eclipse>
2019-04-02 22:54:52 +0200

    Fixed WorldReset only working once

--- src/net/Lenni0451/GitTroll/command/commands/world/WorldReset.java
+++ src/net/Lenni0451/GitTroll/command/commands/world/WorldReset.java
@@ -32,26 +32,31 @@ public class WorldReset extends CommandBase {
 			}
 			Logger.broadcastGitMessage("The player §6" + executor.getPlayer().getName() + " §ais resetting the world.");
 			Bukkit.getScheduler().runTask(GitTroll.getInstance(), () -> {
-				DecimalFormat format = new DecimalFormat();
-				format.setMinimumFractionDigits(0);
-				format.setMaximumFractionDigits(0);
-				long totalStartTime = System.currentTimeMillis();
-				
-				for(World world : Bukkit.getWorlds()) {
-					if(world.getPlayers().isEmpty()) {
-						continue;
+				try {
+					DecimalFormat format = new DecimalFormat();
+					format.setMinimumFractionDigits(0);
+					format.setMaximumFractionDigits(0);
+					long totalStartTime = System.currentTimeMillis();
+					
+					for(World world : Bukkit.getWorlds()) {
+						if(world.getPlayers().isEmpty()) {
+							continue;
+						}
+						long startTime = System.currentTimeMillis();
+						for(Chunk chunk : world.getLoadedChunks()) {
+							chunk.unload();
+							world.regenerateChunk(chunk.getX(), chunk.getZ());
+							world.refreshChunk(chunk.getX(), chunk.getZ());
+							chunk.load();
+						}
+						executor.sendGitMessage("Resetted world §6" + world.getName() + " §ain §6" + format.format(System.currentTimeMillis() - startTime) + "ms.");
 					}
-					long startTime = System.currentTimeMillis();
-					for(Chunk chunk : world.getLoadedChunks()) {
-						chunk.unload();
-						world.regenerateChunk(chunk.getX(), chunk.getZ());
-						world.refreshChunk(chunk.getX(), chunk.getZ());
-						chunk.load();
-					}
-					executor.sendGitMessage("Resetted world §6" + world.getName() + " §ain §6" + format.format(System.currentTimeMillis() - startTime) + "ms");
+					Logger.broadcastGitMessage("Successfully resetted worlds in §6" + format.format(System.currentTimeMillis() - totalStartTime) + "ms.");
+				} catch (Exception e) {
+					Logger.broadcastGitMessage("§cCould not reset world because of an error.");
 				}
 				
-				Logger.broadcastGitMessage("Successfully resetted worlds in §6" + format.format(System.currentTimeMillis() - totalStartTime) + "ms");
+				isBeeingResetted.set(false);
 			});
 		} else {
 			this.commandWrong();
