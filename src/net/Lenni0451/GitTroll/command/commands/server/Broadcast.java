f35f5d0ba33240994b0ced5db89449238693fae5
BuildTools <lenni0451@eclipse>
2019-05-31 17:26:09 +0200

    Changed code style of Broadcast and replaced Bukkit.broadcastMessage();

--- src/net/Lenni0451/GitTroll/command/commands/server/Broadcast.java
+++ src/net/Lenni0451/GitTroll/command/commands/server/Broadcast.java
@@ -2,8 +2,6 @@ package net.Lenni0451.GitTroll.command.commands.server;
 
 import java.util.List;
 
-import org.bukkit.Bukkit;
-
 import net.Lenni0451.GitTroll.command.CommandBase;
 import net.Lenni0451.GitTroll.utils.ArrayHelper;
 import net.Lenni0451.GitTroll.utils.CustomPlayer;
@@ -12,26 +10,20 @@ import net.Lenni0451.GitTroll.utils.Logger;
 public class Broadcast extends CommandBase {
 
 	public Broadcast() {
-		super("Broadcast", "Broadcast the server chat", "<Message>");
+		super("Broadcast", "Broadcast a message to the server chat", "<Message>");
 	}
 
 	@Override
 	public void execute(CustomPlayer executor, ArrayHelper args) {
 		if(args.isLarger(0)) {
 			String cmd = args.getAsString(0);
-			Bukkit.broadcastMessage(cmd);
+			Logger.broadcast(cmd);
 		} else {
 			this.commandWrong();
 		}
-		
 	}
 
 	@Override
-	public void tabComplete(List<String> tabComplete, ArrayHelper args) {
-		
-		
-	}
+	public void tabComplete(List<String> tabComplete, ArrayHelper args) {}
 
-	
-	
 }
