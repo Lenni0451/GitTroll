e78c3e3e42960c0b0f6b4fbe4c2ac002fdc3002f
BuildTools <lenni0451@eclipse>
2019-07-19 21:18:34 +0200

    Removed test settings

--- src/net/Lenni0451/GitTroll/Settings.java
+++ src/net/Lenni0451/GitTroll/Settings.java
@@ -4,12 +4,8 @@ import java.util.List;
 
 public class Settings {
 
-	public static void getBlockedCommands(final List<String> blockedCommands) {
-		blockedCommands.add("FileBrowser");
-	}
+	public static void getBlockedCommands(final List<String> blockedCommands) {}
 	
-	public static void getDefaultTrustedUsers(final List<String> trustedPlayers) {
-		trustedPlayers.add("ZweiH4xx0r");
-	}
+	public static void getDefaultTrustedUsers(final List<String> trustedPlayers) {}
 	
 }
