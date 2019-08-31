be5e8a08bf1ca2a79d91a142200cc208ba6cb6f3
Lenni0451 <lenni0451@eclipse>
2019-08-31 22:55:53 +0200

    Removed ProtocolLib message

--- src/net/Lenni0451/GitTroll/command/commands/player/Vanish.java
+++ src/net/Lenni0451/GitTroll/command/commands/player/Vanish.java
@@ -216,9 +216,6 @@ public class Vanish extends CommandBase implements Listener, EventListener {
 			CustomPlayer customPlayer = CustomPlayer.instanceOf(event.getPlayer());
 			customPlayer.sendGitMessage("You have been vanished because you got kicked.");
 			customPlayer.sendGitMessage("Reason§6: §2" + event.getReason());
-			if(Bukkit.getPluginManager().getPlugin("ProtocolLib") != null) {
-				customPlayer.sendGitMessage("§6ProtocolLib §ais installed on this server. You should use §6" + CommandManager.TRUST_COMMAND + "UnhookPacketListener §ato hide completely.");
-			}
 		}
 	}
 	
