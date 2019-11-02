4df031e89b20fecf1b30fbb3dc3d03acedff9418
Lenni0451 <lenni0451@eclipse>
2019-11-03 00:46:28 +0100

    Added LocationUtils for some easier Location handling

---
+++ src/net/Lenni0451/GitTroll/utils/LocationUtils.java
@@ -0,0 +1,41 @@
+package net.Lenni0451.GitTroll.utils;
+
+import java.text.DecimalFormat;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Material;
+
+public class LocationUtils {
+	
+	private static final DecimalFormat decimalFormat = new DecimalFormat();
+	
+	static {
+		decimalFormat.setMaximumFractionDigits(5);
+		decimalFormat.setGroupingUsed(false);
+	}
+	
+	public static Location getHighestPoint(final Location location) {
+		Location tempLoc = location.clone();
+		while(tempLoc.getBlockY() >= 0) {
+			if(!tempLoc.getBlock().getType().equals(Material.AIR)) return tempLoc;
+			tempLoc.subtract(0, 1, 0);
+		}
+		tempLoc = location.clone();
+		while(tempLoc.getBlockY() <= 255) {
+			if(!tempLoc.getBlock().getType().equals(Material.AIR)) return tempLoc;
+			tempLoc.add(0, 1, 0);
+		}
+		return null;
+	}
+	
+	public static String serialize(final Location location) {
+		return location.getWorld().getName() + " " + decimalFormat.format(location.getX()).replace(",", ".") + " " + decimalFormat.format(location.getY()).replace(",", ".") + " " + decimalFormat.format(location.getZ()).replace(",", ".");
+	}
+	
+	public static Location deserialize(final String string) {
+		String[] parts = string.split(" ");
+		return new Location(Bukkit.getWorld(parts[0]), Double.valueOf(parts[1]), Double.valueOf(parts[2]), Double.valueOf(parts[3]));
+	}
+	
+}
